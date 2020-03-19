a2b40b93b09a44d42f52896d921550abfea3473e
Ryan Stevens <ryan.antony.stevens@gmail.com>
2020-03-19 00:53:20 -0400

    rotating line is working, but line is buggy. The probelm is definitely with my draw_line funtion. I cannot just loop over x to draw the line as it rotates. Think about the case where the line is "vertical." there will be only one x, so how do you plot all the y's in one loop iteration?

--- pixeltoaster/ExampleDrawLine.cpp
+++ pixeltoaster/ExampleDrawLine.cpp
@@ -51,13 +51,26 @@ void plot_point(int x, int y, float r, float g, float b)
 
 void draw_line(int x0, int y0, int x1, int y1)
 {
+    printf("line from (%d, %d) to (%d, %d)\n", x0, y0, x1, y1);
     int y = 0;
     float t = 0.0f;
+
+    if(x1 < x0)
+    {
+        int x1_tmp = x1;
+        int y1_tmp = y1;
+        x1 = x0;
+        y1 = y0;
+        x0 = x1_tmp;
+        y0 = y1_tmp;
+    }
+
     for(int x = x0; x < x1; x++)
     {
-	t = float(x - x0) / float(x1 - x0);
+        t = float(x - x0) / float(x1 - x0);
         y = y0 + ((y1 - y0) * t);
-	plot_point(x, y, 1.0f, 0.5f, 0.25f);
+        printf("x = %d, y = %d\n", x, y);
+        plot_point(x, y, 1.0f, 1.0f, 1.0f);
     } 
 }
 
@@ -66,21 +79,26 @@ int main()
     Display display( "DrawLine Example", width, height, Output::Fullscreen );
 
     float theta = 0.0f; 
+    float half_w = width / 2;
+    float half_h = height / 2;
+    float con_rad = PI / 180.0f;
+    float theta_rad = 0.0f;
 
     while ( display.open() )
     {
-	set_background_color(1.0, 1.0, 1.0);
-
-//	printf("250*-cos(theta*PI/180.0f) = %f, 250*-sin(theta*PI/180.0f) = %f, 500*cos(theta*PI/180.0f) = %f, 500*sin(theta*PI/180.0f = %f\n", 250*-cos(theta*PI/180.0f), 250*-sin(theta*PI/180.0f), 500*cos(theta*PI/180.0f), 500*sin(theta*PI/180.0f));
-	draw_line(250*-cos(theta*PI/180.0f), 250*-sin(theta*PI/180.0f), 500*cos(theta*PI/180.0f), 500*sin(theta*PI/180.0f));
-	display.update( pixels );
-	if(theta < 360.0f)
-	{
-	    theta++;
-	}
-	else
-	{
-	    theta = 0.0f;
-	}
+        set_background_color(0.1, 0.2, 0.5);
+        
+        theta_rad = theta * con_rad;
+        draw_line(half_w * (cos(theta_rad + PI) + 1), half_h * (sin(theta_rad + PI) + 1), half_w * (cos(theta_rad)+1), half_h *(sin(theta_rad)+1));
+        display.update( pixels );
+        printf("theta = %f\n", theta);
+        if(theta < 360.0f)
+        {
+            theta++;
+        }
+        else
+        {
+            theta = 0.0f;
+        }
     }
 }
